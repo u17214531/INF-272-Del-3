adcdf1f663185d15aaf8b558d996706ec8c5590f
PotegoLudwick <50365291+PotegoLudwick@users.noreply.github.com>
2019-10-17 13:41:49 +0200

    Add files via upload
    View models

---
+++ LeaderVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Deliverable_2_WireFrames.Models;
+namespace Deliverable_2_WireFrames.ViewModels
+{
+    public class LeaderVM
+    {
+        public int? CommunityLeaderID;
+        public List<CommunityLeader> leaders;
+        public UserVM userVM;
+
+    }
+}
---
+++ UserVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Deliverable_2_WireFrames.Models;
+
+namespace Deliverable_2_WireFrames.ViewModels
+{
+    public class UserVM
+    {
+        public User user;
+        public void RefreshGUID(OnlinePharmacyEntities db)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            user.GUID = Guid.NewGuid().ToString();
+            user.GUIDExpiry = DateTime.Now.AddMinutes(30);
+            int guids = db.Users.Where(usr => usr.GUID == user.GUID).Count();
+            if (guids > 0)
+                RefreshGUID(db);
+            else
+            {
+                var u = db.Users.Where(zz => zz.UserID == user.UserID).FirstOrDefault();
+                db.Entry(u).CurrentValues.SetValues(user);
+                db.SaveChanges();
+            }
+        }
+
+        public bool IslogedIn(OnlinePharmacyEntities db)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            var guids = db.Users.Where(usr => usr.GUID == user.GUID && usr.GUIDExpiry > DateTime.Now).Count();
+            if (guids > 0)
+                return true;
+            return false;
+        }
+
+        public bool IslogedIn(OnlinePharmacyEntities db, string userGUID)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            user = db.Users.Where(usr => usr.GUID == userGUID && usr.GUIDExpiry > DateTime.Now).FirstOrDefault();
+            if (user != null)
+                return true;
+            return false;
+        }
+    }
+}
